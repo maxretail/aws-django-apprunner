d3cf231868a3854e976bedd168359f359ac33677
DAMON CIARELLI <gdamon@gmail.com>
2025-04-30 14:56:15 -0700

    remove unused decorators

--- apps/core/decorators.py
+++
@@ -1,9 +0,0 @@
-from functools import wraps
-from django.http import JsonResponse
-import asyncio
-
-def async_view(view_func):
-    @wraps(view_func)
-    def _wrapped_view(request, *args, **kwargs):
-        return asyncio.run(view_func(request, *args, **kwargs))
-    return _wrapped_view 
